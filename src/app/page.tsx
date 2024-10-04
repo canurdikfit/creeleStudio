597efa61f810e35634ef2540ddbfb47a3c34590b
canurdikfit <161978264+canurdikfit@users.noreply.github.com>
2024-10-04 16:07:03 +0100

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,36 +10,55 @@ import ServiceS from './components/home/ServiceS'
 import ProjectS from './components/home/ProjectS'
 import VideoS from './components/home/VideoS'
 import AboutS from './components/home/AboutS'
+import Loading from './components/Loading'
+import { useState } from 'react'
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [loaded, setLoaded] = useState<boolean>(true)
 
   return (
     <>
-      <Navbar />
-      <ReactFullpage
-        navigation
-        credits={{
-          enabled: false
-        }}
-        touchSensitivity={5}
-        scrollingSpeed={1000}
-        render={({ fullpageApi }) => {
+      {
+        loaded ? (
+          <Loading
+            loading={loading}
+            onLoad={() => {
+              setLoading(false)
+              setTimeout(() => {
+                setLoaded(false)
+              }, 1000)
+            }}
+          />
+        ) : (
+          <>
+            <Navbar />
+            <ReactFullpage
+              navigation
+              credits={{
+                enabled: false
+              }}
+              touchSensitivity={5}
+              scrollingSpeed={1000}
+              render={({ fullpageApi }) => {
 
-          return (
-            <ReactFullpage.Wrapper>
-              <Banner route={fullpageApi} />
-              <AboutS />
-              <VideoS />
-              <ProjectS />
-              <ServiceS />
-              <Music />
-              <ContactC />
-              <Footer />
-            </ReactFullpage.Wrapper>
-          )
-        }
-        }
-      />
+                return (
+                  <ReactFullpage.Wrapper>
+                    <Banner route={fullpageApi} />
+                    <AboutS />
+                    <VideoS />
+                    <ProjectS />
+                    <ServiceS />
+                    <Music />
+                    <ContactC />
+                    <Footer />
+                  </ReactFullpage.Wrapper>
+                )
+              }
+              }
+            />
+          </>
+        )}
 
 
     </>
